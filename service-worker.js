e3a9a3f1f5a1d0a69421a7c48849fdc93de2bd2c
OliinykKostya <oliinyk.kostya@gmail.com>
2020-06-16 22:22:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.a8da29ae0b3bb62d0315382ff0ec41ad.js"
 );
 
 self.addEventListener('message', (event) => {
